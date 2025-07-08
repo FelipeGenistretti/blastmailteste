423c3802e2159b84922cd208a9d2f96011508cd0
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-07-08 17:43:16 -0300

    Fazendo o front do create links

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -12,21 +12,38 @@
 
                     <form method="GET" action="{{ route('email-list.create') }}" class="flex justify-end mb-4">
                         <button type="submit"
-                            class="px-4 py-2 bg-black text-white rounded hover:bg-gray-900 transition shadow-md !bg-black !text-white">
+                            class="px-4 py-2 bg-black text-white rounded hover:bg-gray-900 transition shadow-md">
                             {{ __('Create new email list') }}
                         </button>
                     </form>
 
-                    <!-- Listagem -->
-                    @forelse($EmailList as $list)
-                        <div class="p-4 border-b border-gray-300">
-                            {{ $list->nome ?? 'Sem nome definido' }}
-                        </div>
-                    @empty
-                        <div class="text-center text-gray-400">
-                            {{ __('No email lists found.') }}
-                        </div>
-                    @endforelse
+                    <!-- Nova tabela -->
+                    <div class="overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
+                        <table class="w-full text-left text-sm text-on-surface dark:text-on-surface-dark">
+                            <thead class="border-b border-outline bg-surface-alt text-sm text-on-surface-strong dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark-strong">
+                                <tr>
+                                    <th scope="col" class="p-4">ID</th>
+                                    <th scope="col" class="p-4">Título</th>
+                                    <th scope="col" class="p-4">Criado em</th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-outline dark:divide-outline-dark">
+                                @forelse($EmailList as $list)
+                                    <tr>
+                                        <td class="p-4">{{ $list->id }}</td>
+                                        <td class="p-4">{{ $list->title }}</td>
+                                        <td class="p-4">{{ $list->created_at->format('d/m/Y') }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3" class="p-4 text-center text-gray-400">
+                                            {{ __('No email lists found.') }}
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
 
                 </div>
             </div>
